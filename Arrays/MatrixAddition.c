904c2873ac2bac5063031aa0648ff9181fe83d0e
Khadiza Rehan <khadizarehan5@gmail.com>
2026-08-04 01:44:23 +0600

    Create MatrixAddition.c

---
+++ Arrays/MatrixAddition.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+int main()
+{
+    int row, column;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &row);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &column);
+
+    if(row <= 0 || column <= 0)
+    {
+        printf("Invalid matrix size");
+        return 0;
+    }
+
+    int matrix1[row][column];
+    int matrix2[row][column];
+    int result[row][column];
+
+    printf("Enter first matrix:\n");
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("Enter second matrix:\n");
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("Added matrix:\n");
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            printf("%d ", result[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
