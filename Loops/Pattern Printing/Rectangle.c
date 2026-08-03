e4a5561b38e19a8800939c2352d35857d4de1e4f
Khadiza Rehan <khadizarehan5@gmail.com>
2026-08-04 01:54:58 +0600

    Create Rectangle.c

---
+++ Loops/Pattern Printing/Rectangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int row, column;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &row, &column);
+
+    for(int i = 1; i <= row; i++)
+    {
+        for(int j = 1; j <= column; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
