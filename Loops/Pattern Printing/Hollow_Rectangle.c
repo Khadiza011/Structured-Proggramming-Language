2322e5eca7ad5e8ac2ecb2d8324833efa16bbbd3
Khadiza Rehan <khadizarehan5@gmail.com>
2026-08-04 02:04:51 +0600

    Create Hollow_Rectangle.c

---
+++ Loops/Pattern Printing/Hollow_Rectangle.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int row, column;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &row, &column);
+
+    for(int i = 1; i <= row; i++)
+    {
+        for(int j = 1; j <= column; j++)
+        {
+            if(i == 1 || i == row || j == 1 || j == column)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
