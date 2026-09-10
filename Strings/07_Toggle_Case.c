f02797b3dac7624ebfb77a4a8dd8e9fc55f56076
Khadiza Rehan <khadizarehan5@gmail.com>
2026-09-10 17:37:15 +0600

    Add toggle case feature in 07_Toggle_Case.c
    Implement toggle case functionality for a string input.

---
+++ Strings/07_Toggle_Case.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d",&n);
+
+    char str[n];
+
+
+    getchar();
+
+    gets(str);
+
+
+
+    int i;
+
+
+    for(i=0; str[i]!='\0'; i++)
+    {
+
+        if(str[i]>='a' && str[i]<='z')
+        {
+            str[i]=str[i]-32;
+        }
+
+        else if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i]=str[i]+32;
+        }
+
+    }
+
+
+
+    printf("%s",str);
+
+
+
+    return 0;
+}
